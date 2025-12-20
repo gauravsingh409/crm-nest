a4ec1a95dc3fac58551f343264d5911d6d26016e
Gaurav Kushwaha <gaurav.kushwaha.0825@gmail.com>
2025-12-20 15:23:54 +0545

    feat: get all lead api

--- src/lead/lead.controller.ts
+++ src/lead/lead.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ResponseService } from 'src/common/response/response.service';
 import { LeadService } from './lead.service';
+import { CreateLeadDto } from './dto/create-lead.dto';
+import { PaginationDto } from 'src/common/pagination.dto';
 
 @Controller('lead')
 export class LeadController {
@@ -10,13 +20,19 @@ export class LeadController {
   ) {}
 
   @Get('/')
-  async getAllLead() {}
+  async getAllLead(@Query() pagination: PaginationDto) {
+    const { meta, records } = await this.leadService.getAllLead(pagination);
+    return this.responseService.pagination(records, meta);
+  }
 
   @Get('/:id')
   async getLeadDetails() {}
 
   @Post('/')
-  async createLead() {}
+  async createLead(@Body() request: CreateLeadDto) {
+    const lead = await this.leadService.createLead(request);
+    return this.responseService.success(lead, 'Lead created successfully', 201);
+  }
 
   @Patch('/')
   async updateLead() {}
--- src/lead/lead.service.ts
+++ src/lead/lead.service.ts
@@ -1,16 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PaginationDto } from 'src/common/pagination.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateLeadDto } from './dto/create-lead.dto';
+import { ResponseService } from 'src/common/response/response.service';
 
 @Injectable()
 export class LeadService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private responseService: ResponseService,
+  ) {}
 
   async leadDetails(id: string) {}
 
-  async getAllLead(pagination: PaginationDto) {}
+  async getAllLead(pagination: PaginationDto) {
+    const { limit, page } = pagination;
+    const skip = (page - 1) * limit;
+    const [lead, total] = await Promise.all([
+      this.prismaService.lead.findMany({
+        skip,
+        take: limit,
+        orderBy: { createdAt: 'asc' },
+      }),
+      this.prismaService.lead.count(),
+    ]);
+    return {
+      records: lead,
+      meta: this.responseService.paginationMetaData(total, page, limit),
+    };
+  }
 
-  async createLead() {}
+  async createLead(request: CreateLeadDto) {
+    try {
+      const lead = await this.prismaService.lead.create({
+        data: {
+          firstName: request.firstName,
+          lastName: request.lastName,
+          email: request.email,
+          phone: request.phone,
+          whatsapp: request.whatsapp,
+          address: request.address,
+          dob: request.dob ? new Date(request.dob) : undefined,
+          categories: request.categories,
+          service: request.service,
+          source: request.source,
+          description: request.description,
+        },
+      });
+
+      return lead;
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to create lead');
+    }
+  }
 
   async updateLead(id: string) {}
 
