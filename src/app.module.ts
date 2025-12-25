ca66f03b78e1f76e0a7eb1af0890d83464f5a03b
Gaurav Kushwaha <gaurav.kushwaha.0825@gmail.com>
2025-12-25 19:37:50 +0545

    feat: branch module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { BootstrapModule } from './bootstrap/bootstrap.module';
-import { UserModule } from './user/user.module';
-import { LeadModule } from './lead/lead.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { LeadModule } from './modules/lead/lead.module';
+import { BranchModule } from './modules/branch/branch.module';
 
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
+    UserModule,
+    LeadModule,
+    BranchModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     BootstrapModule,
-    UserModule,
-    LeadModule,
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), 'uploads'),
       serveRoot: '/uploads',
